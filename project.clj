56ffc17cd3ae6946019bc88997b376af27fc2f75
Joshua Crean <joshua.crean@gmail.com>
2011-07-26 22:30:47 -0400

    initial lein project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dapper "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.unboundid/unboundid-ldapsdk "2.0.0" ]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/dapper/test/core.clj
@@ -0,0 +1,6 @@
+(ns dapper.test.core
+  (:use [dapper.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
