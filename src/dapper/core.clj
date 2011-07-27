d59997f2ead500574f22a5faa582e2d6418a8441
Joshua Crean <joshua.crean@gmail.com>
2011-07-27 13:17:47 -0400

    adding basic functions needed to establish LDAPConnectionPools and bind to the server

--- src/dapper/core.clj
+++ src/dapper/core.clj
@@ -4,12 +4,18 @@
   (:use
    [clj-etl-utils.lang-utils  :only [raise]])
   (:import
-   [com.unboundid.ldap.sdk LDAPConnection LDAPConnectionPool]))
+   [com.unboundid.ldap.sdk
+    LDAPConnection LDAPConnectionPool RoundRobinServerSet
+    SimpleBindRequest SingleServerSet]))
 
 
 (defonce *connection-registry* (atom {}))
 
 (def ^:dynamic *current-connection* nil)
+
+(def *default-pool-size* 5)
+(def *default-port* 389)
+(def *default-ssl-port* 636)
 
 (defn- raise-unregistered-connection! [conn-name]
   (raise "Error: no LDAP connection registered with name [%s], registered names are [%s]"
@@ -20,10 +26,75 @@
     (raise "LDAP Connection already registered under %s" kwd)
     (swap! *connection-registry* assoc kwd (atom {:connection nil :config cfg}))))
 
-;; NB: need to actually make the connection, just testing registration for now
+(defn unregister-ldap! [kwd]
+  (if-let [conf (get @*connection-registry* kwd)]
+    (do
+      (when-let [conn (:connection @conf)]
+        (.close conn))
+      (swap! *connection-registry* dissoc kwd))
+    (raise-unregistered-connection! kwd)))
+
+(defn reregister-ldap! [kwd cfg]
+  (when (get @*connection-registry* kwd)
+    (unregister-ldap! kwd))
+  (register-ldap! kwd cfg))
+
+
+(defn configured-hosts [config]
+  (concat
+   (if (vector? (:host config))
+     (:host config)
+     (vector (:host config)))
+   (:hosts config)))
+
+(defn server-info [host-str]
+  (let [host        (str/replace host-str #"^ldaps?://" "")
+        [host port] (str/split host #":")
+        port        (str/replace (or port *default-port*) #"\D*$" "")]
+    {:host host
+     :port (if (empty? port)
+             *default-port*
+             (Integer/parseInt port))}))
+
+(defn round-robin-server-set [config]
+  (let [servers (map server-info (configured-hosts config))]
+    (RoundRobinServerSet.
+     (into-array String (map :host servers))
+     (int-array (map :port servers)))))
+
+(defn single-server-set [config]
+  (let [info (server-info (:host config))]
+   (SingleServerSet.
+    (:host info)
+    (:port info))))
+
+(defn create-server-set [config]
+  (cond (or (:hosts config)
+            (vector? (:host config)))
+        (round-robin-server-set config)
+
+        (string? (:host config))
+        (single-server-set config)
+
+        :else
+        (raise "Error: Don't know how to create server-set from config: %s" config)))
+
+(defn bind-dn [config]
+  (or (:bind-dn config)
+      (format "uid=%s,%s" (:bind-user config) (:user-dn-suffix config))))
+
+(defn create-connection-pool [config]
+  (create-server-set *conf*)
+  (LDAPConnectionPool.
+   (create-server-set config)
+   (SimpleBindRequest. (bind-dn config) (:bind-pass config))
+   (get config :pool-size *default-pool-size*)))
+
 (defn create-connection [config]
   (println "Creating new connection....")
-  (LDAPConnection.))
+  (if (:pooled? config)
+    (create-connection-pool config)
+    (raise "Unimplented! non-pooled connections not yet implemented.")))
 
 (defn ldap-connect! [conn-name]
   (if-let [cfg (get @*connection-registry* conn-name)]
@@ -59,9 +130,19 @@
 
 (comment
 
-  (register-ldap!
-   :dapper {:host "your-host"
-            :user-dn-suffix "ou=users,dc=company,dc=com"})
+  (reregister-ldap!
+   :dapper {:host "ec2-184-72-160-16.compute-1.amazonaws.com"
+            :bind-user "jcrean"
+            :bind-pass "jcjcjc"
+            :user-dn-suffix "ou=users,dc=relayzone,dc=com"
+            :pooled? true
+            :pool-size 3})
+
+  (round-robin-server-set
+   {:host "ec2-184-72-160-16.compute-1.amazonaws.com"
+    :user-dn-suffix "ou=users,dc=relayzone,dc=com"
+    :pooled? true
+    :pool-size 3})
 
   (with-ldap :dapper
     (printf "current-connection: %s" *current-connection*))
