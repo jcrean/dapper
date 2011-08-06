ae07fe5fdcfcccde471d75c22a64418e374f4435
Joshua Crean <joshua.crean@gmail.com>
2011-08-06 00:52:06 -0400

    corrected provider configuration for replication. also removed hardcoded hostnames from deploy.sh

--- chef/deploy.sh
+++ chef/deploy.sh
@@ -4,37 +4,18 @@
 # This was taken mostly from http://opinionated-programmer.com/2011/06/chef-solo-tutorial-managing-a-single-server-with-chef/. 
 #
 
-LDAP01=ec2-184-72-140-230.compute-1.amazonaws.com
-LDAP02=ec2-107-20-41-167.compute-1.amazonaws.com
-
 if [ -z "$1" ]; then
-  echo "Supply host to provision"
+  echo "Supply role to use for provisioning (master or slave)"
   exit -1
 fi
 
-case "$1" in
-    ldap01)
-        echo "Provisioning ldap01..."
-        HOST=$LDAP01
-        ROLE='master'
-        ;;
-    ldap02)
-        echo "Provisioning ldap02..."
-        HOST=$LDAP02
- 	ROLE='slave'
-        ;;
-    *)
-        echo "Unsupported host ID: $1"
-        exit 1
-esac
+ROLE=$1
+HOST=${2:-localhost}
+
+echo "Provisioning new $ROLE LDAP server at $HOST"
 
 rsync -avz . ec2-user@$HOST:/home/ec2-user/chef/
 
-ssh -t ec2-user@$HOST 'cd chef; sudo ./install.sh $ROLE;'
+echo "ssh -t ec2-user@$HOST 'cd chef; sudo ./install.sh $ROLE;'"
+ssh -t ec2-user@$HOST "cd chef; sudo ./install.sh $ROLE"
 
-# tar cj . | ssh -o 'StrictHostKeyChecking no' ec2-user@"$HOST" '
-# rm -rf ~/chef &&
-# mkdir ~/chef &&
-# cd ~/chef &&
-# tar xj &&
-# bash install.sh'
